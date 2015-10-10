0dce47509b63d7f949d5f92807707ef11e62dd16
Hemang Rajyaguru <hemdagem@gmail.com>
2015-10-10 14:44:43 +0100

    added exercises

---
+++ Exercise1/src/Test.js
@@ -0,0 +1,9 @@
+function addingnumbers(first,second){
+return first+second;
+};
+
+describe("Add numbers", function () {
+    it("adds the numbers", function () {
+        //add code to create a test
+    });
+});
---
+++ Exercise3/src/Test.js
@@ -0,0 +1,21 @@
+var calculator=  {
+      addNumbers: function(firstNumber, secondNumber) {
+			return firstNumber + secondNumber;
+      }
+}
+
+describe("A spy", function() {
+
+  beforeEach(function() {
+    
+	//code to setup
+ 
+	});
+
+  it("tracks that the spy was called", function() {
+
+	//code to check method was called
+
+	});
+    
+});
